2c100e2dd087c3afacafc1809db8665cec0aefe2
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-03-25 18:56:59 +0700

    quiz2_3 ไม่ได้

--- quiz2_3.c
+++ quiz2_3.c
@@ -1,46 +1,40 @@
 #include<stdio.h>
 int main()
 {
-    long long n,count=1,save;
+    long long n,count=0,save,give=1;
     scanf("%lld",&n);
     save=n;
-    while (n>=10)
+    for(long long i=1;i<=n;i++)
     {
-        n=n/10;
-        count=count*10;
-    }
-    count++;
-    int ans=0;
-    if(count<=11)
-    {
-        for(int i=1;i<=save;i++)
-        {
-            ans++;
+        if(i<10)
+        {   
+            if(i==give)
+                give=give*10;
+            count++;
         }
-        if(ans==10)
-            ans--;
-    }
-    else if(count<=101)
-    {
-        for(int i=11;i<=save;i=i+11)
+        else if (i>=10&&i<100)
         {
-            ans++;
-        }
-        ans=ans+9;
-    }
-    else 
-    {
-        for(int i=100;i<1000;i++)
-        {
-            long long king1=i%10,king2=i%100,king3;
-            king3=(i%1000-king2)/100;
-            if(king1==king3)
+            if(i==give)
             {
-                ans++;
+                give=give*10;
             }
+            long long het1=i%(give/10),het2;
+            het2=(i-het1)/(give/10);
+            if(het1==het2)
+                count++;
         }
-        ans=ans+18;
+        else if(i>=100&&i<1000)
+        {
+            if(i==give)
+            {
+                give=give*10;
+            }
+            long long het1=i%(give/10),het2;
+            het2=(i-het1)/(give/10);
+            if(het1==het2)
+                count++;
+        }
     }
-    printf("%d",ans);
+    printf("%lld",count);
     return 0;
 }
