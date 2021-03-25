b0c197a29ce9ce61d5dd95b5fded95a686fa1c70
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-03-25 19:40:32 +0700

    ทำใหม่

--- quiz2_3.c
+++ quiz2_3.c
@@ -1,38 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    long long n,count=0,save,give1,give2;
+    long long n;
     scanf("%lld",&n);
-    save=n;
-    for(long long i=1;i<=n;i++)
-    {
-        if(i<10)
-        {
-            count++;
-            give1=11;
-        }
-        if(i>=10&&i<100)
-        {
-            if(i==give1)
-            {
-                give1=give1+11;
-                count++;
-            }
-            give2=101;
-        }
-        if(i>=100&&i<1000)
-        {
-            if(i==give2)
-            {
-                for(int j=1;j<=9;j++)
-                    count++;
-            }
-            else
-            {
-                give2=give2+10;
-            }
-        }
-    }
-    printf("%lld",count);
+    
     return 0;
 }
