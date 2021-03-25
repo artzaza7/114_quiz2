8700f3334036555b5137d55eae3fe1981bf9dc6b
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-03-25 19:43:56 +0700

    ได้ 10 กับ 100 แล้วครับ

--- quiz2_3.c
+++ quiz2_3.c
@@ -1,8 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    long long n;
+    long long n,count=0;
     scanf("%lld",&n);
-    
+    for(long long i=1;i<=n;i++)
+    {
+        if(i<10)
+        {
+            count++;
+        }
+        else if(i>=10&&i<100)
+        {
+            if(i%11==0)
+            {
+                count++;
+            }
+        }
+    }
+    printf("%lld",count);
     return 0;
 }
